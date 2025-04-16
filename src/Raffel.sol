73e81438c47da0c16da32e2735142fe84524362d
Smitbhuva15 <itssmit123@gmail.com>
2025-04-16 09:55:59 +0000

    modife Raffel.sol

--- src/Raffel.sol
+++ src/Raffel.sol
@@ -1,4 +1,3 @@
-
 // Layout of Contract:
 // version
 // imports
@@ -20,24 +19,76 @@
 // private
 // view & pure functions
 
-
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-contract Raffel {
+import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
 
-error Raffle__SendMoreToEnterRaffle();
+contract Raffel is VRFConsumerBaseV2 {
+    error Raffle__SendMoreToEnterRaffle();
+
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
 
     uint256 private immutable i_entranceFee;
+    uint256 private immutable i_interval;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
 
-    constructor(uint256 entranceFee) {
+    address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
+
+    // Events
+
+    event EnteredRaffel(address indexed palyer);
+
+    constructor(
+        uint256 entranceFee,
+        uint256 interval,
+        address vrfCoordinator,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinator) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinator);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
     function RaffelFun() external payable {
-        if(msg.value<i_entranceFee){
-             revert Raffle__SendMoreToEnterRaffle(); 
+        if (msg.value < i_entranceFee) {
+            revert Raffle__SendMoreToEnterRaffle();
         }
+        s_players.push(payable(msg.sender));
+        emit EnteredRaffel(msg.sender);
+    }
+
+    function PickWinner() external {
+        if (block.timestamp - s_lastTimeStamp < i_interval) {
+            revert();
+        }
+
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        
     }
 
     /**
